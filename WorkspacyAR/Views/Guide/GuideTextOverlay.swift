63556f776d4db83364cbe35f2397e7130b3e33de
bomsiwor <bomsiwor@gmail.com>
2026-07-20 16:05:15 +0700

    feat: guide overlay component
    (cherry picked from commit f751a3a2af0337b78a544d58da2187c4c5d57900)

---
+++ WorkspacyAR/Views/Guide/GuideTextOverlay.swift
@@ -0,0 +1,33 @@
+//
+//  GuideTextOverlay.swift
+//  WorkspacyAR
+//
+//  Created by Bomanarakasura on 20/07/26.
+//
+
+import SwiftUI
+
+struct GuideTextOverlay: View {
+    
+    let caption: String
+    
+    var body: some View {
+        Text(caption)
+//            .font(.caption)
+            .foregroundColor(.primary.opacity(0.9))
+            .padding(.horizontal, 12)
+            .padding(.vertical, 6)
+            .background(.regularMaterial)
+//            .background(Color.black.opacity(0.6))
+            .cornerRadius(10)
+            .padding(.bottom, 16)
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.gray
+        
+        GuideTextOverlay(caption: "Tap object surface to adjust the position")
+    }
+}
